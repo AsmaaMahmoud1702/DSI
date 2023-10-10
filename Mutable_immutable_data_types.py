fcf245824b831793e2760e505d90166e5b956e32
AsmaaMahmoud1702 <amohamed0001702@alumni.focuscollege.com>
2023-10-10 12:21:58 -0700

    Create Mutable_immutable_data_types.py
    Immutable and Mutable datatypes

---
+++ Mutable_immutable_data_types.py
@@ -0,0 +1,13 @@
+# String is a immutable data type
+name = "asmaa"
+name= "asmaa mahmoud"
+print(type(name))
+# print(name)
+
+# list is a mutable data type
+
+list1 = ["hello"]
+list1.append("world")
+print(list1)
+print(type(list1))
+
